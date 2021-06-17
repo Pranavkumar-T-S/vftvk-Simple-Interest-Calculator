19725a6e91d717281635b53527cdf1115b0bf52c
Pranavkumar-T-S <pranavtspk@gmail.com>
2021-06-18 00:46:48 +0530

    changes are made as per the requirements

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+function slide(val)
+{
+document.getElementById("percent").innerHTML=val+'%';
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = Number(document.getElementById("principal").value),
+    rate = Number(document.getElementById("rate").value),
+    year = Number(document.getElementById("years").value);
+    if(p<=0){
+    window.alert("Enter a postive amount ");
+    document.getElementById('principal').focus();
+    }
+    else{
+    var amount=(p*year*rate/100)+p;
+    statement='If you deposit <mark>'+p+'</mark>,<br> at an interest rate of <mark>'+rate+'%</mark>.<br> You will receive an amount of <mark>'+amount+'</mark>,<br> in the year <mark>'+(Number(new Date().getFullYear())+year)+'</mark>';
+    document.getElementById('result').innerHTML=statement; 
+    }
 }
         
